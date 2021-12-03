82541fb7e9de97d4439cba66aabf9351773ccead
johnindra31 <john.indra31@gmail.com>
2021-12-03 16:48:39 +0700

    file baru

---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam consequuntur aperiam, cupiditate harum amet eligendi doloremque unde, autem molestiae repellat rem ratione. Nobis maiores repellendus atque quam, voluptatibus iure officia?</p>
+
+        </div>
+    </div>
+</div>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hello Dr. Awang</h1>
+
+        </div>
+    </div>
+</div>
